63563caebf1f734da28bf3463f72b184f7c2ceb3
kate brenner <kate.e.brenner@gmail.com>
2018-03-11 10:31:07 -0400

    lines moving on road

--- src/Game.java
+++ src/Game.java
@@ -2,13 +2,15 @@ import processing.core.PApplet;
 
 public class Game {
     PApplet p;
-    String mState = "wait";
+    String mState = "go";
+    int roadX = 50;
     Game(PApplet p, String State){
         this.p = p;
         mState = State;
     }
 
     public void render() {
+        roadX = roadX - 1;
         p.background(124,252,0);
         p.stroke(100, 100, 100);
         p.fill(100,100,100);
@@ -18,9 +20,9 @@ public class Game {
         p.rect(0, 500, 1024, 200);
         p.stroke(255, 255, 0);
         p.fill(255, 255, 0);
-        for (int i = 0; i < 1024; i = i + 100) {
-            p.rect(i, 200, 50, 10);
-            p.rect(i, 600, 50, 10);
+        for (int i = 1; i < 10000; i = i + 100) {
+            p.rect(i + roadX, 200, 50, 10);
+            p.rect(i + roadX, 600, 50, 10);
         }
     }
 
